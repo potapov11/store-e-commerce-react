508a42e8507e275e720ee0a8a781160c8b5ae151
Serezha <volos11223344@yandex.ru>
2024-05-19 19:12:46 +0300

    добавляет форму

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -10,18 +10,49 @@ export function Form() {
 	} = useForm({ mode: 'onChange' });
 
 	const onSubmit = (data) => {
+		reset();
 		console.log(data);
 	};
 
 	// console.log(watch('example')); // watch input value by passing the name of it
 
 	return (
-		<form onSubmit={handleSubmit(onSubmit)} style={{ opacity: '0' }}>
-			<input placeholder="name" {...register('name', { required: true })} />
-			<input placeholder="e-mail" type="email" {...register('email', { required: true })} />
-			{/* {errors.exampleRequired && <span>This field is required</span>} */}
-			<button disabled={!isValid}>Отправить</button>
-		</form>
+		<div className="form__modal">
+			<div className="form__inner-modal">
+				<form onSubmit={handleSubmit(onSubmit)}>
+					<label>
+						<input
+							placeholder="name"
+							type="text"
+							{...register('name', {
+								required: true,
+								minLength: {
+									value: 2,
+									message: 'Минимум 5 символов',
+								},
+							})}
+						/>
+						<div style={{ height: '40px', color: 'red' }}>{errors?.name && <p>{errors?.name?.message}</p>}</div>
+					</label>
+					<label>
+						<input
+							placeholder="e-mail"
+							type="email"
+							{...register('email', {
+								required: 'Введите email',
+								pattern: {
+									value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+									message: 'Неправильный email',
+								},
+							})}
+						/>
+						<div style={{ height: '40px', color: 'red' }}>{errors?.email && <p>{errors?.email?.message}</p>}</div>
+					</label>
+					{/* {errors.exampleRequired && <span>This field is required</span>} */}
+					<button disabled={!isValid}>Отправить</button>
+				</form>
+			</div>
+		</div>
 	);
 }
 
