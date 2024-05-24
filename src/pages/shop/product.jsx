08ffbf80b0e59056c61cf3388d8987d6718acb7b
s.potapov <s.potapov@severnaya.ru>
2024-05-24 11:51:52 +0300

    заканчивает выпадашку сортировку

--- src/pages/shop/product.jsx
+++ src/pages/shop/product.jsx
@@ -1,24 +1,41 @@
-import React, { useContext } from 'react';
-import { ShopContext } from '../../context/shop-context';
+import React, { useContext, useState, useEffect } from "react";
+import star from "../../assets/img/star.svg";
+import { ShopContext } from "../../context/shop-context";
 
 export function Product(props) {
-	const { addToCart, cartItems } = useContext(ShopContext);
+  const { addToCart, cartItems } = useContext(ShopContext);
+  const [countStars, setCountStars] = useState([]);
 
-	const { data } = props;
-	const { id, productName, price, productImage } = data;
+  const { data } = props;
+  const { id, productName, price, productImage, rating } = data;
 
-	return (
-		<div className="product no_highlight">
-			<img src={productImage} alt="product-img" />
-			<div className="description">
-				<b>
-					<p>{productName}</p>
-				</b>
-				<p>${price}</p>
-			</div>
-			<button className="addToCartBttn" onClick={() => addToCart(id)}>
-				add to cart {cartItems[id] > 0 && cartItems[id]}
-			</button>
-		</div>
-	);
+  useEffect(() => {
+    const stars = [];
+    for (let i = 0; i < rating; i++) {
+      stars.push(i);
+    }
+    setCountStars(stars);
+  }, [rating]);
+
+  return (
+    <div className="product no_highlight">
+      <img src={productImage} alt="product-img" />
+      <div className="description">
+        <b>
+          <p>{productName}</p>
+        </b>
+        <div className="product-info">
+          <p>${price}</p>
+          <div className="product__rating">
+            {countStars.map((item) => {
+              return <span className="product__star"></span>;
+            })}
+          </div>
+        </div>
+      </div>
+      <button className="addToCartBttn" onClick={() => addToCart(id)}>
+        add to cart {cartItems[id] > 0 && cartItems[id]}
+      </button>
+    </div>
+  );
 }
--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -3,8 +3,9 @@ import "./shop.css";
 import { PRODUCTS } from "../../product";
 import { Product } from "./product";
 const sortOptionsArr = [
-  { value: "plus-sort", text: "сортировать по возрастанию" },
-  { value: "minus-sort", text: "сортировать по убыванию" },
+  { value: "plus-sort", text: "сортировать по возрастанию цены" },
+  { value: "minus-sort", text: "сортировать по убыванию цены" },
+  { value: "rating-sort", text: "сортировать по рейтингу" },
 ];
 
 export function Shop() {
@@ -17,6 +18,8 @@ export function Shop() {
       PRODUCTS.sort((a, b) => a.price - b.price);
     } else if (e.target.value === "plus-sort") {
       PRODUCTS.sort((a, b) => b.price - a.price);
+    } else if (e.target.value === "rating-sort") {
+      PRODUCTS.sort((a, b) => b.rating - a.rating);
     }
   }
 
@@ -26,15 +29,19 @@ export function Shop() {
         <div className="shopTitle">
           <h1>Tecno-shop</h1>
         </div>
-        <select name="sort" id="sort" onChange={handleChange}>
-          {sortOptionsArr.map((item) => (
-            <option value={item.value}>{item.text}</option>
-          ))}
-        </select>
-        <div className="products">
-          {PRODUCTS.map((product) => (
-            <Product key={product.id} data={product} />
-          ))}
+        <div className="product-wrapper">
+          <div className="sort-block">
+            <select name="sort" id="sort" onChange={handleChange}>
+              {sortOptionsArr.map((item) => (
+                <option value={item.value}>{item.text}</option>
+              ))}
+            </select>
+          </div>
+          <div className="products">
+            {PRODUCTS.map((product) => (
+              <Product key={product.id} data={product} />
+            ))}
+          </div>
         </div>
       </div>
     </div>
